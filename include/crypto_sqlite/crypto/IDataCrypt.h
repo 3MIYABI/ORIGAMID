cf776295f21db57de965cc3cb417de16c393f889
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2020-09-13 21:25:33 +0000

    drm: Destroy the planes prior to destroying the associated CRTC

--- include/crypto_sqlite/crypto/IDataCrypt.h
+++ include/crypto_sqlite/crypto/IDataCrypt.h
@@ -13,4 +13,5 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU L
+ * You should have received a copy of the GNU Lesser General Public License
+ * along wi
