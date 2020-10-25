6e77b1d071c636558861a4717b33787e624c84d8
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2020-10-25 22:38:37 +0000

    fixing image

--- include/crypto_sqlite/crypto/IDataCrypt.h
+++ include/crypto_sqlite/crypto/IDataCrypt.h
@@ -27,4 +27,4 @@ class IDataCrypt {
 public:
     virtual ~IDataCrypt() = default;
 
-    virtual void encrypt(uint32_t page, const Buffer &source, Buffe
+    virtual void encrypt(uint32_t page, const Buffer &source, Buffer &destination, const Buffer &ke
