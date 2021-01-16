5143d39a883746aab0c156d41b7284e423c4d5be
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2021-01-16 20:29:10 +0000

    Updated copyright notice

--- include/crypto_sqlite/crypto/PlaintextCrypt.h
+++ include/crypto_sqlite/crypto/PlaintextCrypt.h
@@ -4,4 +4,5 @@
  * This file is part of CryptoSQLite.
  *
  * CryptoSQLite is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public Lice
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Soft
