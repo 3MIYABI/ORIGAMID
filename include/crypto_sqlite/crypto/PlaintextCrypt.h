fc50993e662aa3f761346df0d18035a1f9c85906
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2021-03-30 15:04:08 +0000

    Trying to fix travis build

--- include/crypto_sqlite/crypto/PlaintextCrypt.h
+++ include/crypto_sqlite/crypto/PlaintextCrypt.h
@@ -10,4 +10,4 @@
  *
  * CryptoSQLite is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABIL
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR 
