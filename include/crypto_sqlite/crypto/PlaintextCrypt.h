ca40e8b326690bc4684bc0602bcd72b08e56397e
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2021-04-08 06:17:39 +0000

    Docs typo

--- include/crypto_sqlite/crypto/PlaintextCrypt.h
+++ include/crypto_sqlite/crypto/PlaintextCrypt.h
@@ -13,4 +13,4 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
- * You should have received a c
+ * You should have received a copy of the GNU Lesser General
