1603cf2d59b7dc9ba55a56fb8ae0c11dda53e05e
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2021-04-09 09:36:27 +0000

    updated packages

--- include/crypto_sqlite/crypto/PlaintextCrypt.h
+++ include/crypto_sqlite/crypto/PlaintextCrypt.h
@@ -13,4 +13,5 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General
+ * You should have received a copy of the GNU Lesser General Public License
+ * along wit
