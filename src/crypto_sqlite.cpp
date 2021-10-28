3250b2627fa82f83741f5fc2cd75e76e8a21e46d
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2021-10-28 20:15:55 +0000

    More readme changes

--- src/crypto_sqlite.cpp
+++ src/crypto_sqlite.cpp
@@ -66,4 +66,5 @@ int sqlite3_rekey_encrypted(const char *zFilename, const void *zKeyOld, int nKey
 }
 
 int sqlite3_key(sqlite3* db, const void*, int) {
-    // The key is only set for the main database, not the temp dat
+    // The key is only set for the main database, not the temp database
+    const char *fileName =
