34dbf3bed88619a7ea12280545519ddc31fc416a
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2021-11-15 21:06:27 +0000

    target: Allow AllRegistrants to re-RESERVE existing reservation

--- src/csqlite/SQLite3Mutex.h
+++ src/csqlite/SQLite3Mutex.h
@@ -4,4 +4,4 @@
  * This file is part of CryptoSQLite.
  *
  * CryptoSQLite is free software: you can redistribute it and/or modify
- * it under the terms of t
+ * it under the terms of the GNU Lesser General Public 
