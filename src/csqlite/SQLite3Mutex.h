bbeab78dbd4027eef54c477ef3eeeb82a71c8285
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2021-11-26 18:57:57 +0000

    adds .travis.yml

--- src/csqlite/SQLite3Mutex.h
+++ src/csqlite/SQLite3Mutex.h
@@ -6,4 +6,6 @@
  * CryptoSQLite is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
- * (at your option) an
+ * (at your option) any later version.
+ *
+ * Cry
