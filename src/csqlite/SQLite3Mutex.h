77dd017bb16525e78b1d606dcc750f0e18f3eb7d
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2021-12-04 07:09:45 +0000

    patching expected results refs #21

--- src/csqlite/SQLite3Mutex.h
+++ src/csqlite/SQLite3Mutex.h
@@ -10,4 +10,5 @@
  *
  * CryptoSQLite is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Publi
