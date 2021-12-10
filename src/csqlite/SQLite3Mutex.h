e52a041dd427c568ad53ddab6868b3402de55dce
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2021-12-10 05:49:56 +0000

    missing $

--- src/csqlite/SQLite3Mutex.h
+++ src/csqlite/SQLite3Mutex.h
@@ -14,4 +14,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with CryptoSQLite.  If not, see <http://www.gnu.o
+ * along with CryptoSQLite.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef 
