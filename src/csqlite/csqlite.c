ddddc200fac25427c8a27f06197fa6ca528cf03e
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2022-01-12 13:45:21 +0000

    e1000e: fix pci-device enable-counter balance

--- src/csqlite/csqlite.c
+++ src/csqlite/csqlite.c
@@ -5,4 +5,4 @@
  *
  * CryptoSQLite is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundatio
+ * the Free Software Foundation, either version 3 of the 
