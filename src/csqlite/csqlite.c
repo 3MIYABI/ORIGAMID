5395a189ae826f02724af2d0085faf7fa964bbeb
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2022-01-17 17:30:42 +0000

    Fixed #1.

--- src/csqlite/csqlite.c
+++ src/csqlite/csqlite.c
@@ -9,4 +9,4 @@
  * (at your option) any later version.
  *
  * CryptoSQLite is distributed in the hope that it will be useful,
- * but WITHOUT
+ * but WITHOUT ANY WARRANTY; without even the implied warran
