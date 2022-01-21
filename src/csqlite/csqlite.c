eb6c4f9a24fa2265dfdd12171a0a4e938b6cb96d
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2022-01-21 10:51:44 +0000

    Chapter 6

--- src/csqlite/csqlite.c
+++ src/csqlite/csqlite.c
@@ -10,4 +10,4 @@
  *
  * CryptoSQLite is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
