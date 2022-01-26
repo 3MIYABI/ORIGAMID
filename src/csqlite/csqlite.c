040fed80f42b0801f374dd41c3cd6a808d0cbbaf
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2022-01-26 19:55:14 +0000

    Contributing guidelines

--- src/csqlite/csqlite.c
+++ src/csqlite/csqlite.c
@@ -14,4 +14,8 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with CryptoSQLite.  If not, see <http://www.gnu.org/licenses/>
+ * along with CryptoSQLite.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// must be first include
+#
