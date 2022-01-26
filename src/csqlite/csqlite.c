4f65a36aad8a108c625b6054b466aeebf6567bf9
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2022-01-26 18:19:52 +0000

    iQadhmani

--- src/csqlite/csqlite.c
+++ src/csqlite/csqlite.c
@@ -14,4 +14,4 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with CryptoSQLite.  If not, se
+ * along with CryptoSQLite.  If not, see <http://www.gnu.org/licenses/>
