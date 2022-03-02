c6538c9421551437152c0951f29888ebcbe55d02
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2022-03-02 19:01:56 +0000

    Fix usage example

--- src/csqlite/csqlite.h
+++ src/csqlite/csqlite.h
@@ -5,4 +5,4 @@
  *
  * CryptoSQLite is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the 
+ * the Free Software Foundation,
