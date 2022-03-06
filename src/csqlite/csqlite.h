0952fa9e1f2af97eb856a3d56a2cf142641f3c87
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2022-03-06 11:47:51 +0000

    New translations translation.json (Portuguese)

--- src/csqlite/csqlite.h
+++ src/csqlite/csqlite.h
@@ -10,4 +10,4 @@
  *
  * CryptoSQLite is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABI
+ * MERCHANTABILITY or FITNESS FOR A PARTICULA
