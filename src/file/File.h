37ddfe4a0cda371f76131fbff0c166d136bb86f4
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2022-03-25 21:17:52 +0000

    secure

--- src/file/File.h
+++ src/file/File.h
@@ -24,3 +24,45 @@
 #include <type_traits>
 #include <vector>
 #include <string>
+#include "../crypto/Crypto.h"
+
+extern "C" {
+#include <sqlite3.h>
+};
+
+const int SQLITE_OPEN_MASK = SQLITE_OPEN_MAIN_DB |
+                             SQLITE_OPEN_TEMP_DB |
+                             SQLITE_OPEN_TRANSIENT_DB |
+                             SQLITE_OPEN_MAIN_JOURNAL |
+                             SQLITE_OPEN_TEMP_JOURNAL |
+                             SQLITE_OPEN_SUBJOURNAL |
+                             SQLITE_OPEN_MASTER_JOURNAL |
+                             SQLITE_OPEN_WAL;
+
+const int SQLITE_WAL_FRAMEHEADER_SIZE = 24;
+
+class File {
+public:
+    /* Constructor/destructor can not be used because sqlite3 manages this memory */
+
+    int attach(sqlite3 *db, int nDB);
+
+    int close();
+    int read(void* buffer, int count, sqlite3_int64 offset);
+    int write(const void* buffer, int count, sqlite3_int64 offset);
+
+protected:
+    int readMainDB(void *buffer, int count, sqlite3_int64 offset);
+    int readJournal(void *buffer, int count, sqlite3_int64 offset);
+    int readWal(void *buffer, int count, sqlite3_int64 offset);
+
+    int writeMainDB(const void *buffer, int count, sqlite3_int64 offset);
+    int writeJournal(const void *buffer, int count, sqlite3_int64 offset);
+    int writeWal(const void *buffer, int count, sqlite3_int64 offset);
+
+public:
+    sqlite3_file mBase;
+    /**/
+    sqlite3_file *mUnderlying;
+    const char *mFileName;
+    int mExists;
