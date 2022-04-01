505cb52155b76d12c38db88c9e47955ce2514876
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2022-04-01 14:09:49 +0000

    Session

--- src/vfs/VFS.cpp
+++ src/vfs/VFS.cpp
@@ -3,4 +3,5 @@
  *
  * This file is part of CryptoSQLite.
  *
- * CryptoSQLite is free software: you can redistribute it and/or
+ * CryptoSQLite is free software: you can redistribute it and/or modify
+ * it under the terms of the
