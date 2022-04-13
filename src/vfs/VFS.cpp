8595acc0777fa085ecf62af06456dfe09ebf4fbb
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2022-04-13 09:46:39 +0000

    Fix a few issues

--- src/vfs/VFS.cpp
+++ src/vfs/VFS.cpp
@@ -11,4 +11,4 @@
  * CryptoSQLite is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU 
+ * GNU Lesser General Public Licen
