3c88d06e313a1ff0b91154634e6a39ab8a364df8
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2022-04-18 17:34:02 +0000

    Add Travis-CI badge

--- src/vfs/VFS.cpp
+++ src/vfs/VFS.cpp
@@ -14,4 +14,5 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with CryptoSQLite.  I
+ * along with CryptoSQLite.  If not, see <http://www.gnu.org/licenses/>.
+ 
