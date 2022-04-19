50ee2806505b781fa4a0879b183fe07b92bb7a28
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2022-04-19 13:28:53 +0000

    Updated feedstocks submodules. [ci skip]

--- src/vfs/VFS.cpp
+++ src/vfs/VFS.cpp
@@ -15,4 +15,6 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with CryptoSQLite.  If not, see <http://www.gnu.org/licenses/>.
- 
+ */
+
+#include <algorithm>
