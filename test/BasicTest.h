801c47d1a4f58adcf8a167922a11dcab4996849e
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2022-10-18 13:15:52 +0000

    remove return

--- test/BasicTest.h
+++ test/BasicTest.h
@@ -35,3 +35,14 @@ protected:
 
     void testReadWrite(const char *key, int keylen, bool transact = false, int insertCount = 1000) {
         testWrite(key, keylen, transact, insertCount);
+        testRead(key, keylen, insertCount);
+    }
+
+    void testWrite(const char *key, int keylen, bool transact = false, int insertCount = 1000);
+    void testRead(const char *key, int keylen, int insertCount = 1000);
+    void testRekey(const char *oldkey, int oldkeylen, const char *newkey, int newkeylen);
+    void testOpenFail(const char *key, int keylen);
+};
+
+
+#endif //CRYPTOSQLITE_BASICTEST_H
