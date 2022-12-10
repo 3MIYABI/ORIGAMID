d7c571c07830273e3fcdea2ea1ac4cad907b6b50
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2022-12-10 07:27:04 +0000

    print removed

--- test/TestCrypt.h
+++ test/TestCrypt.h
@@ -4,4 +4,5 @@
  * This file is part of CryptoSQLite.
  *
  * CryptoSQLite is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public Lic
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foun
