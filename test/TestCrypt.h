8c7cc705dcd90f2603a8a5312d687363aa1aace2
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2023-01-05 11:18:50 +0000

    ignore *~

--- test/TestCrypt.h
+++ test/TestCrypt.h
@@ -24,4 +24,5 @@
 
 class TestCrypt : public IDataCrypt {
 public:
-    void encrypt(uint32_t page, const Buffer &source, Buffer &destination, const Buffer &k
+    void encrypt(uint32_t page, const Buffer &source, Buffer &destination, const Buffer &key) const override {
+        // cop
