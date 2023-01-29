816fdbc77911d080a7425a5bb0c2613f6294e76f
3MIYABI <105447171+3MIYABI@users.noreply.github.com>
2023-01-29 16:01:16 +0000

    Console commit

--- test/TestCrypt.h
+++ test/TestCrypt.h
@@ -33,4 +33,5 @@ public:
     }
 
     void decrypt(uint32_t page, const Buffer &source, Buffer &destination, const Buffer &key) const override {
-        // since XOR operation is the same, just encrypt again to de
+        // since XOR operation is the same, just encrypt again to decrypt
+        encrypt(page, source, desti
